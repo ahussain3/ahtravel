8ebce1fc63c0e0c386591e930b810869366b64ee
Awais Hussain <unxmnd@gmail.com>
2015-10-14 19:01:04 +0100

    Rough server (list and post) works. Tested with telnet

--- app.js
+++ app.js
@@ -1,49 +1,15 @@
-var mongoose = require('mongoose');
-var moment = require('moment');
-mongoose.connect('mongodb://localhost/india');
+var express = require('express');
+var app = express();
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+app.get('/', function(req, res){
+	res.send('Hello World!');
+});
 
-db.once('open', function (callback) {
-  	var where_now_schema = mongoose.Schema({
-	    message: String,
-	    last_updated: Date //{ type: Date, default: Date.now }
-	});
+var server = app.listen(3000, function () {
+	var host = server.address().address;
+	var port = server.address().port;
 
-// 	kittySchema.methods.speak = function () {
-// 	  var greeting = this.name
-// 	    ? "Meow name is " + this.name.full
-// 	    : "I don't have a name";
-// 	    greeting += " and I am " + this.age + " years old.";
-// 	  console.log(greeting);
-// 	}
-
-	var Where_Now_Obj = mongoose.model('Where_Now', where_now_schema);
-
-	var test = new Where_Now_Obj({
-		message: "London, UK", 
-		last_updated: Date.now()
-	});
-
-	test.save(function(err, test) {
-		if (err) return console.error(err);
-		else console.log("save successful");
-	});
-
-	Where_Now_Obj.findOne(function(err, where) {
-		if (err) return console.error(err);
-		// console.log(where);
-		console.log(where.message);
-		console.log("updated " + moment(where.last_updated).fromNow());
-	});
-
-// 	Kitten.findOne({"age": 4}, function(err, koda) {
-// 		if (err) return console.error(err);
-// 		console.log(koda.name.full);
-// 		koda.speak();
-// 	});
+	console.log('Example app listening at http://%s:%s', host, port);
 
 });
 
-console.log("Hello World!");
